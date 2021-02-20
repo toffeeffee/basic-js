676056eccc25b0ea6f275afeb00d6c88298c5e9a
Irina Komarova <toffee@ukr.net>
2021-02-20 20:17:44 +0200

    init: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeam = new Array;
+  let name = '';
+  let finallyName = '';
+  let char;
+  let fl = false;
+
+  for(i=0;i<members.length;i++){
+    if(typeof(members[i])!='string'){}
+      else{
+          name = members[i].toString();
+          name = name.trim();
+          dreamTeam[i]=name[0].toUpperCase();
+          fl=true;
+        };
+    };
+
+  if(fl==true){
+    dreamTeam.sort();
+    name=dreamTeam.toString();
+    for(i=0;i<name.length;i++){
+      char=name.charCodeAt(i);
+      if (char>=65 && char<=90){
+          finallyName = finallyName + name[i]; 
+        }
+    };
+    return(finallyName);
+  }
+  else{return(false);}
 };
