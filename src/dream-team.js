40ad042689e824ced61f3d542ed5f8c11f68a0d0
Irina Komarova <toffee@ukr.net>
2021-02-23 18:20:23 +0200

    fix: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -6,27 +6,30 @@ module.exports = function createDreamTeam(members) {
   let finallyName = '';
   let char;
   let fl = false;
-
-  for(i=0;i<members.length;i++){
-    if(typeof(members[i])!='string'){}
-      else{
-          name = members[i].toString();
-          name = name.trim();
-          dreamTeam[i]=name[0].toUpperCase();
-          fl=true;
-        };
-    };
-
-  if(fl==true){
-    dreamTeam.sort();
-    name=dreamTeam.toString();
-    for(i=0;i<name.length;i++){
-      char=name.charCodeAt(i);
-      if (char>=65 && char<=90){
-          finallyName = finallyName + name[i]; 
-        }
-    };
-    return(finallyName);
+  if(Array.isArray(members) === false){return false;} 
+  else{
+    for(i=0;i<members.length;i++){
+      if(typeof(members[i])!='string'){}
+        else{
+            name = members[i].toString();
+            name = name.trim();
+            dreamTeam[i]=name[0].toUpperCase();
+            fl=true;
+          };
+      };
+  
+    if(fl==true){
+      dreamTeam.sort();
+      name=dreamTeam.toString();
+      for(i=0;i<name.length;i++){
+        char=name.charCodeAt(i);
+        if (char>=65 && char<=90){
+            finallyName = finallyName + name[i]; 
+          }
+      };
+      return(finallyName);
+    }
   }
-  else{return(false);}
+
+
 };
