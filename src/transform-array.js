c492fbc2b337f649372ae39ab350da1a3183c07c
Irina Komarova <toffee@ukr.net>
2021-02-23 15:53:23 +0200

    init: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+ const action =['--discard-next','--discard-prev','--double-next','--double-prev'];
+  let arrNew=[];
+  if(!Array.isArray(arr)){throw "Error";}
+  else{
+      for(i=0;i<arr.length;i++){arrNew[i]=arr[i];}; 
+      for(index=0;index<arrNew.length;index++){
+          for (i=0;i<action.length;i++){
+              if (arrNew[index]==action[i]){
+                  switch (action[i]){
+                  case '--discard-next':
+                      if(index==arrNew.length-1){arrNew.splice(index, 1);} 
+                      else{arrNew.splice(index, 2);};
+                      break;
+                  case '--discard-prev':
+                      if(index==0){arrNew.splice(index, 1);}
+                      else{arrNew.splice(index-1, 2);}; 
+                      break;
+                  case '--double-next':
+                      if(index==arrNew.length-1){arrNew.splice(index, 1);} 
+                      else{arrNew[index]=arrNew[index+1]};
+                      break;
+                  case '--double-prev':
+                      if(index==0){arrNew.splice(index, 1);}
+                      else{arrNew[index]=arrNew[index-1];}; 
+                      break;
+                  }
+
+              }
+          }
+      }
+      return(arrNew);
+  };
 };
