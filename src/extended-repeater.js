dceb121ebd081346abe4b2d3f8cd96bb4efe7f20
Irina Komarova <toffee@ukr.net>
2021-03-12 16:14:09 +0200

    init: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let newStr ='';
+  let additionStr = '';
+  let additionRepeatTimes;
+  let additionSeparator;
+  let addition;
+  let repeatTimes;
+  let separator;
   
+  if (options.addition !== undefined ){addition = options.addition;}
+    else {addition = '';}
+  if (options.additionRepeatTimes !== undefined ){additionRepeatTimes = options.additionRepeatTimes;}
+    else {additionRepeatTimes = 0;}
+  if (options.additionSeparator !== undefined ){additionSeparator = options.additionSeparator;}
+    else {additionSeparator = '|';}  
+  if (options.repeatTimes !== undefined ){repeatTimes = options.repeatTimes;}
+    else {repeatTimes = 1;}  
+  if (options.separator !== undefined ){separator = options.separator;}
+    else {separator = '+';}     
+  if (str !== undefined ){str = String(str);}
+    else {str = '';}  
+    
+  additionStr=addition;
+  for (i=additionRepeatTimes-1;i>0;i--){additionStr=additionStr+additionSeparator+addition;}
+  newStr=str+additionStr;
+  for(i=repeatTimes-1;i>0;i--){
+      newStr = newStr+separator+str+additionStr;
+  }
+  return(newStr);
+};
